2a1b680e9465054c6a6919540e7088c2456d111e
Tombo Yuuko <dan.quinlog@gmail.com>
2020-10-06 15:16:46 -0600

    began project build, installed react, clean slate at this point.

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# mortonsgift
+website for a build-shop hobbiest for work display and freelance networking
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      hey now, you're an allstar
+    </div>
+  );
+}
+
+export default App;

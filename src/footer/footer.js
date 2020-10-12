bfbc37c8b1b4423bdddbce4b9076a975d79a68ca
Tombo Yuuko <dan.quinlog@gmail.com>
2020-10-11 23:38:03 -0600

    completed redesign of navbar and footernav to eliminate hamburger button and provide clear call to action

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,10 +1,13 @@
-import { waitForElementToBeRemoved } from '@testing-library/react';
-import React from 'react';
+import React from "react";
 
-export default function() {
-  return(
-    <div className='footer'>
-      <div className='footer__body'>call to action -- contact us</div>
+export default function () {
+  return (
+    <div className="footer">
+      <div className="footer__links">
+        <div className="footer__links__link">link1</div>
+        <div className="footer__links__link">link2</div>
+      </div>
+      <div className="footer__c2a">call to action -- contact us</div>
     </div>
   );
 }
--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,54 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      navlinks: false,
-    };
-    this.toggleNavBar = this.toggleNavBar.bind(this);
-  }
-  toggleNavBar() {
-    this.setState({
-      navlinks: !this.state.navlinks,
-    });
-  }
-  closeNavBar() {
-    this.setState({
-      navlinks: false,
-    });
-  }
-  render() {
-    return (
-      <div className="navbar">
-        <div className="navbar__logo">
-          <img
-            className="project__logo__image"
-            src="http://via.placeholder.com/150x50"
-          />
-        </div>
-        <div className="navbar__spacer"></div>
-        <div
-          className={`navbar__links ${
-            this.state.navlinks ? "" : "navbar__hidden"
-          }`}
-        >
-          <div className="navbar__links__link"><img
-            className="project__logo__image"
-            src="http://via.placeholder.com/150x50"
-          /></div>
-          <div className="navbar__links__link"><img
-            className="project__logo__image"
-            src="http://via.placeholder.com/150x50"
-          /></div>
-        </div>
-        <div className="navbar__burgerbutton" onClick={this.toggleNavBar}>
-          <img
-            className="project__logo__image"
-            src="http://via.placeholder.com/50x50"
-          />
-        </div>
+export default function () {
+  return (
+    <div className="navbar">
+      <div className="navbar__logo">
+        <img
+          className="project__logo__image"
+          src="http://via.placeholder.com/150x50"
+          alt="logo"
+        />
       </div>
-    );
-  }
+      <div className="navbar__spacer"></div>
+      <div className="navbar__links">
+        <div className="navbar__links__link">link1</div>
+        <div className="navbar__links__link">link2</div>
+      </div>
+      <div className="navbar__c2a">
+        <span>Contact Us</span>
+      </div>
+    </div>
+  );
 }
